5d5864254eb00c607b2242d5f33d0d5f222f2068
Mitchell Dalzin <dalzin.mitchell@gmail.com>
2023-01-09 11:34:54 -0600

    Made win check work

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,7 +4,7 @@ import { color, Event } from "../types";
 import Tile from "./Tile";
 import useWindowEvent from "../hooks/useWindowEvent";
 import useDocumentEvent from "../hooks/useDocumentEvent";
-import { getBoardColors } from "../helpers/boardColorGenerator";
+import { boardsAreEqual, getBoardColors, shuffleColors } from "../helpers/colorHelpers";
 
 const Container = styled.div`
   position: absolute;
@@ -16,39 +16,14 @@ const Container = styled.div`
   transform: translate(-50%, -50%);
 `
 
-const rows = 8;
-const cols = 10;
-
-function shuffleColors(colors: color[][]): color[][] {
-  const cols = colors.length;
-  const rows = colors[0].length;
-
-  const newColors: (color | undefined)[][] = Array.from({ length:cols }, () => (Array.from({ length:rows }, () => undefined)));
-
-  newColors[0][0] = colors[0].shift();
-  if (rows > 1) newColors[0][rows - 1] = colors[0].pop();
-  if (cols > 1) newColors[cols - 1][0] = colors[cols - 1].shift();
-  if (rows > 1 && cols > 1) newColors[cols - 1][rows - 1] = colors[cols - 1].pop();
-
-  let remainingColors = colors.flat();
-
-  for(let x = 0; x < cols; x++) {
-    for(let y = 0; y < rows; y++) {
-      if (newColors[x][y] === undefined) {
-        const removalIndex = Math.floor(Math.random() * remainingColors.length);
-        newColors[x][y] = remainingColors.splice(removalIndex, 1)[0];
-      }
-    }
-  }
-
-  return newColors as color[][];
-}
-
-const initialBoard = shuffleColors(getBoardColors(rows, cols));
+const rows = 4;
+const cols = 4;
 
 export default function Board() {
 
-  const [colors, setColors] = useState(initialBoard);
+  const solutionBoard = useRef(getBoardColors(rows, cols));
+
+  const [colors, setColors] = useState(shuffleColors(solutionBoard.current));
   const [tileSize, setTileSize] = useState([0, 0]);
 
   const boardRef = useRef<HTMLDivElement>(null);
@@ -63,6 +38,10 @@ export default function Board() {
 
   useEffect(() => {
     updateTileSize();
+  }, [])
+
+  useEffect(() => {
+    if (boardsAreEqual(solutionBoard.current, colors)) alert('You Win!');
   }, [colors])
 
   function updateTileSize() {
@@ -77,16 +56,14 @@ export default function Board() {
   }
 
   function swapTiles(endPosition: [number, number], endColor: color) {
-    setColors(prevColors => {
-      const newColors = [...prevColors];
+    if (selectedPosition.current && selectedColor.current) {
+      const newColors = colors.map(col => [...col]);
 
-      if (selectedPosition.current && selectedColor.current) {
-        newColors[selectedPosition.current[0]][selectedPosition.current[1]] = endColor;
-        newColors[endPosition[0]][endPosition[1]] = selectedColor.current;
-      }
+      newColors[selectedPosition.current[0]][selectedPosition.current[1]] = endColor;
+      newColors[endPosition[0]][endPosition[1]] = selectedColor.current;
 
-      return newColors;
-    })
+      setColors(newColors);
+    }
   }
 
   return (
@@ -95,7 +72,7 @@ export default function Board() {
         colors.flatMap((colorCol, x) => {
           return colorCol.map((color, y) => 
           {
-            const isCorner = (x === 0 || x === cols -1) && (y === 0 || y === rows -1)
+            const isCorner = (x === 0 || x === cols -1) && (y === 0 || y === rows -1);
             return <Tile key={`${x}${y}`} color={color} position={[x, y]} size={tileSize} select={() => selectTile([x,y], color)} swap={() => swapTiles([x,y], color)} isCorner={isCorner}/>
           })
         })
--- src/helpers/colorHelpers.ts
+++ src/helpers/colorHelpers.ts
@@ -55,3 +55,48 @@ export function getBoardColors(rows: number, cols: number): color[][] {
   const bottomRow = interpolateColors(bottomLeft, bottomRight, cols);
   return topRow.map((topColor, i) => interpolateColors(topColor, bottomRow[i], rows));
 }
+
+export function shuffleColors(colors: color[][]): color[][] {
+
+  const oldColors = colors.map(col => [...col]);
+
+  const cols = oldColors.length;
+  const rows = oldColors[0].length;
+
+  const newColors: (color | undefined)[][] = Array.from({ length:cols }, () => (Array.from({ length:rows }, () => undefined)));
+
+  newColors[0][0] = oldColors[0].shift();
+  if (rows > 1) newColors[0][rows - 1] = oldColors[0].pop();
+  if (cols > 1) newColors[cols - 1][0] = oldColors[cols - 1].shift();
+  if (rows > 1 && cols > 1) newColors[cols - 1][rows - 1] = oldColors[cols - 1].pop();
+
+  let remainingColors = oldColors.flat();
+
+  for(let x = 0; x < cols; x++) {
+    for(let y = 0; y < rows; y++) {
+      if (newColors[x][y] === undefined) {
+        const removalIndex = Math.floor(Math.random() * remainingColors.length);
+        newColors[x][y] = remainingColors.splice(removalIndex, 1)[0];
+      }
+    }
+  }
+
+  return newColors as color[][];
+}
+
+export function boardsAreEqual(board1: color[][], board2: color[][]): boolean {
+  const board1Array = board1.flat();
+  const board2Array = board2.flat();
+
+  for (let i = 0; i < board1Array.length; i++) {
+    if (!colorsAreEqual(board1Array[i], board2Array[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function colorsAreEqual(color1: color, color2: color): boolean {
+  return color1.r === color2.r && color1.g === color2.g && color1.b === color2.b;
+}
